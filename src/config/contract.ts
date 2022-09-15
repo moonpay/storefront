80195cfbb2aa13ea0627b4432a0d42e67cd99fec
Jono Kercher <jonathan@hypermint.com>
2022-09-15 09:27:26 +0100

    Update contract to use Goerli

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -3,11 +3,11 @@
 const { NFTContractType, NetworkEnvironment, NetworkType, NetworkChain } = HyperMint;
 
 export default {
-    contractId: '6aba68a3-3da3-4725-954e-a6afc58ea2db',
-    contractAddress: '0x34831c2ed56f2bd50d55d4518d17c05e70174e6c',
+    contractId: 'd8ba7e40-3efe-448f-94e2-e3f53518ac64',
+    contractAddress: '0x1143eb49cee89a42ba4ed61a83b8ce333a3ca083',
     contractType: NFTContractType.ERC1155,
     networkEnvironment: NetworkEnvironment.Testnet,
-    networkType: NetworkType.Polygon,
-    networkChain: NetworkChain.Mumbai,
+    networkType: NetworkType.Ethereum,
+    networkChain: NetworkChain.Goerli,
     enableLogging: true
 };
