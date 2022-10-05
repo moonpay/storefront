f24343fac252f19f2ef40a422f6806cb7faaaea7
Ryan Fogle <Ryan Fogle>
2022-10-05 18:51:08 -0400

    update contract for storefront.hypermint.com

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -3,8 +3,8 @@
 const { NFTContractType, NetworkEnvironment, NetworkType, NetworkChain } = HyperMint;
 
 export default {
-    contractId: 'd8ba7e40-3efe-448f-94e2-e3f53518ac64',
-    contractAddress: '0x1143eb49cee89a42ba4ed61a83b8ce333a3ca083',
+    contractId: '35a7b61a-763b-446b-916a-8cf24b262477',
+    contractAddress: '0xaec2959723c9e662b3ca56e27b3c1136aabfdd80',
     contractType: NFTContractType.ERC1155,
     networkEnvironment: NetworkEnvironment.Testnet,
     networkType: NetworkType.Ethereum,
