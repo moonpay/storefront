ef5bac02949ebd7d3ba0ba3c7dd4f917bd5089ce
Jono Kercher <jonathan@hypermint.com>
2023-02-07 09:48:25 +0000

    hmint-902 change contract to mainnnet

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -1,13 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const { NFTContractType, NetworkEnvironment, NetworkType, NetworkChain } = HyperMint;
-
 export default {
-    contractId: '35a7b61a-763b-446b-916a-8cf24b262477',
-    contractAddress: '0xaec2959723c9e662b3ca56e27b3c1136aabfdd80',
-    contractType: NFTContractType.ERC1155,
-    networkEnvironment: NetworkEnvironment.Testnet,
-    networkType: NetworkType.Ethereum,
-    networkChain: NetworkChain.Goerli,
+    contractId: 'ee20b68e-356c-4547-bac1-2ccee24a47cd',
     enableLogging: true
 };
